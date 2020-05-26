57ca61123aedaa6e0a4e5b4259af455a1299a8a0
Wilson (Fengchi) Wang <wilsonfwang@gmail.com>
2020-05-26 14:40:19 -0400

    [Update] (1.10b0) - Https
    Release Notes:
    
    - Nothing Impressive just added Https as Default

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var httpApp = express();
+var fs = require('fs');
 var http = require('http')
+var server = require('https').createServer({
+    key: fs.readFileSync("/etc/letsencrypt/live/chemtoy.space/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/chemtoy.space/cert.pem")
+}, app).listen(443);
+var path = require('path');
 
-app.set('port', 80);
+httpApp.set('port', 80);
+httpApp.get("*", function(req, res, next) {
+    res.redirect("https://" + req.headers.host);
+});
 
 app.use(express.urlencoded({
     extended: true
@@ -10,10 +20,15 @@ app.use(express.urlencoded({
 
 app.use(express.static(__dirname + '/public'))
 
+
+app.get('/toy', function(req, res) {
+    res.sendFile(__dirname + '/public/toy.html');
+});
+
 app.get('*', function(req, res) {
     res.status(404).sendFile(__dirname + '/public/404.html');
 });
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express HTTP server listening on port ' + app.get('port'));
+http.createServer(httpApp).listen(httpApp.get('port'), function() {
+    console.log('Express HTTP server listening on port ' + httpApp.get('port'));
 });
